336aa8fd2143772fda7b2592a31b54bdb4016782
ajanthan <ajanthan.kt@gmail.com>
2020-04-19 10:44:38 +1000

    Use graphql-yoga to write basic hello world queries
    const typeDefs=`
        type Query{
            hello:String!
        } `
    
    const resolvers={
        Query:{
            hello(){
                return 'this is my first query'
            }
        }
    
    const sever=new GraphQLServer({
        typeDefs:typeDefs,
        resolvers:resolvers
    })
    sever.start(()=>{
        console.log("The server is up and running");
    })

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-import addition,{subtract} from './math';
+import {GraphQLServer} from 'graphql-yoga';
 
-console.log("Hello World Graph QL");
-console.log("Addition ",addition(10,5));
-console.log("Subtract ",subtract(10,5));
+const typeDefs=`
+    type Query{
+        hello:String!,
+        name:String!,
+        location:String!,
+        bio:String!
+    }      
+`
+
+const resolvers={
+    Query:{
+        hello(){
+            return 'this is my first query'
+        },
+        name(){
+            return 'ajanthan' 
+        },
+        bio(){
+            return 'some bio information' 
+        },
+        location(){
+            return 'location information' 
+        }
+    }
+}
+
+const sever=new GraphQLServer({
+    typeDefs:typeDefs,
+    resolvers:resolvers
+})
+
+sever.start(()=>{
+    console.log("The server is up and running");
+})
